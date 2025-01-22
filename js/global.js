7325a4aab35e00f7115829255aa811c5eac86a68
BalletDeLaCiudadDeMerida <balletdelaciudaddemerida@gmail.com>
2025-01-22 13:32:36 -0600

    Update global.js

--- js/global.js
+++ js/global.js
@@ -1,3 +1,12 @@
+// Seleccionar el botón del menú hamburguesa y el contenedor del menú
+const menuToggle = document.querySelector(".menu-toggle");
+const nav = document.querySelector("header nav");
+
+// Escuchar el evento "click" en el botón hamburguesa
+menuToggle.addEventListener("click", () => {
+    nav.classList.toggle("open");
+});
+
 console.log("El archivo script.js se está ejecutando correctamente.");
 
 function cerrarCintilla() {
